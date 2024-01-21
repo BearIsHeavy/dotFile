c0d23b0952d07ac2d54cbb08121d250a60412fdd
Q.Q.Bear <2650183131@qq.com>
2024-01-22 07:48:10 +0800

    prepare git push

--- Initial.sh
+++ Initial.sh
@@ -46,8 +46,7 @@ create_nvm() {
     if [[ -s "$HOME/.nvm" || -d "$HOME/.nvm" ]];then
         echo -e "you had have nvm"
     else
-       read -r "Do you decided install nvm in this computer" dec
-       echo "$dec"
+       read -r "Do you decide to install nvm in this computer" dec
        if [[ $dec != "no" && $dec != "N" && $dec != "n" ]];then
            code="curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash"
           if [[ $code == 0 ]];then
