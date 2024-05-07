710b57aa09af36f363b203640919ecf0e67c3049
bear <2650183131@qq.com>
2024-05-07 09:50:21 +0800

    version 1.1.5 prepare public version1.1.1

--- initial/nvim.sh
+++ initial/nvim.sh
@@ -1,37 +1,33 @@
 #!/bin/env bash
 
+source ~/.dotfile/initial/colors.sh
+
 install_nvm() {
-  if [[ -s "$HOME/.nvm" || -d "$HOME/.nvm" ]];then
-    echo -e "you had have nvm"
-  else
-    read -r -p "Do you decide to install nvm in this computer(y/n): " dec
-    if [[ $dec =~ ^(y|Y) ]];then
-      ( 
-        tar -xvzf "$HOME"/.dotfile/Download/nvim-linux64.tar.gz \
-          && mv nvim-linux64/bin/nvim "$HOME"/bin \
-          && rm -rf nvim-linux64
-      ) || exit 2
-    fi
+  read -r -p "Do you decide to install neovim in this computer(y/n): " dec
+  if [[ $dec =~ ^(y|Y) ]];then
+    ( cd ~/.dotfile/Download \
+        && tar -xvzf "$HOME"/.dotfile/Download/nvim-linux64.tar.gz \
+        && sudo mv nvim-linux64/ /opt/nvim-linux64 \
+    ) || exit 2
   fi
 }
 
 initial_nvim_config() {
   # create .config/nvim format
   if [[ -d $HOME/.config/nvim || -L $HOME/.config/nvim ]]; then
-    echo -e "nvim config file existed, do you want to override it? \n"
-    read -p -r "yes/no" ans
-    if [[ $ans =~ ^(n|N) ]]; then
-      exit 0
+    echo -e "\n ${RED}nvim config file existed, do you want to override it?${RESET} \n"
+    read -r -p "yes/no: " ans
+    if [[ $ans =~ ^(y|Y) ]]; then
+      # make .config direcotry
+      ( rm -rf "$HOME"/.config/nvim \
+          && mkdir -p "$HOME"/.config/ \
+          && ln -s "$HOME"/.dotfile/nvim "$HOME"/.config 1>/dev/null)
     fi
-
-    # make .config direcotry
-    ( rm -rf "$HOME"/.config/nvim \
-        && make -p "$HOME"/.config/nvim \
-        && cp -r "$HOME"/.dotfile/nvim "$HOME"/.config )
   else
-    make -p "$HOME"/.config/nvim && ln -s "$HOME"/.dotfile/nvim "$HOME"/.config
+    echo -e -n "Installing...\n"
+    mkdir -p "$HOME"/.config/nvim && ln -s "$HOME"/.dotfile/nvim "$HOME"/.config
   fi
-  echo -e "please access nvim/lua/plugins/plugins-setup.lua the type :PackerSync"
+  echo -e "${GREEN}please access nvim/lua/plugins/plugins-setup.lua the type :PackerSync \n${RESET}"
 }
 
 
@@ -39,7 +35,10 @@ initial_nvim_config() {
 echo "Whether to create stand-form directory link for Neovim: "
 read -r -p "yes/no: " ans
 if [[ $ans =~ ^(Y|y) ]];then
-   install_nvm && initial_nvim_config || echo "Initial NVIM Fail"; exit 3
+   install_nvm || exit 3
+   initial_nvim_config || exit 4
 elif [[ $ans =~ ^[N|n] ]];then
     echo -e "No Link be created\n"
 fi
+
+
--- initial/otherComamnd.sh
+++ initial/otherComamnd.sh
@@ -3,19 +3,20 @@
 . $HOME/.dotfile/initial/colors.sh # import colors
 
 # install tmux command
-sudo apt install tmux -y || exit 2
+sudo apt install tmux -y || exit 2 && echo -e -r "${GREEN}\ntmux download successfully ${RESET}\n"
 
 # install tldr command
-sudo apt install tldr || exit 3
+sudo apt install tldr || echo -e -n "${RED}\ntldr install faile${RESET}\n" && echo -e -n "${GREEN}\ntldr success ${RESET} \n"
 if tldr test > /dev/null 2>&1;then
-  echo -e -n "${RED}tldr --update fail${RESET}" 1>&2
-  exit 4 
+  echo -e -n "${RED}\ntldr --update fail${RESET}" 1>&2
 fi
 
 # install shellcheck
-if sudo apt install shellcheck;then
-  echo -e -n "${RED}shellcheck faile${RESET}" 1>&2
-  exit 5
+if ! sudo apt install shellcheck;then
+  echo -e -n "${RED}\n shellcheck faile\n${RESET}" 1>&2
+  echo -e "\n"
+else
+  echo -e -n "\n${GREEN}shellcheck install successfully${RESET}\n"
 fi
 
 # install exa comamnd
@@ -24,10 +25,12 @@ if [[ $version_ubuntu -ge 24 ]]; then
   sudo apt install exa || exit 1
 else
   ( mkdir -p "$HOME"/Download \
-    cd "$HOME"/Download/ \
+    && cd "$HOME"/Download/ \
+    && wget --timeout=5 --tries=1 https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip \
     && wget https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip \
     && sudo apt install unzip  \
     && unzip exa-linux-x86_64-0.9.0.zip \
     && mv exa-linux-x86_64 ~/bin \
-    && rm -rf "$HOME"/Download ) || exit 2
+    && rm -r "$HOME"/Download/exa-linux-x86_64 ) || echo -e -n "${RED}exa command install fail${RESET}\n"; exit 3
+  echo -e -n "${GREEN}exa command install successfully${RESET}\n"
 fi
