9b7eea909d6125e3fc903110b0ba24dabb1027a5
QuanQing <2650183131@qq.com>
2026-02-15 22:06:28 +0800

    update neovide config file

--- nvim/lua/plugins/neovide.lua
+++ nvim/lua/plugins/neovide.lua
@@ -2,16 +2,21 @@
 if vim.g.neovide then
   -- Helper function for transparency formatting
   local alpha = function()
-    return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
+    return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
   end
 
-  -- g:neovide_transparency should be 0 
-  -- if you want to unify transparency of content 
-  -- and title bar.
-  vim.g.neovide_transparency = 0.8
+  -- UPDATED: neovide_transparency is deprecated.
+  -- We use neovide_opacity (0.0 to 1.0) instead.
+  vim.g.neovide_opacity = 0.8
+  
+  -- Internal variable used for the background color calculation below
   vim.g.transparency = 0.65
+
   vim.g.neovide_theme = 'dark'
+  
+  -- Setting background color with alpha (Hex format #RRGGBBAA)
   vim.g.neovide_background_color = "#0f1117" .. alpha()
+  
   vim.g.neovide_fullscreen = false
   vim.g.neovide_cursor_vfx_mode = "pixiedust"
 end
